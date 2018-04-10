ec796beae8e71b20631f613a917a8f19fec028b4
Aldwin Delgado Dev-02@DevKinetics <aldwinpaolodelgado@gmail.com>
2018-04-10 12:37:15 +0800

    Add dummy feign-client service example

---
+++ hello-feign-client/src/main/java/com/github/aldwindelgado/hellofeignclient/HelloFeignClientApplication.java
@@ -0,0 +1,12 @@
+package com.github.aldwindelgado.hellofeignclient;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloFeignClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloFeignClientApplication.class, args);
+	}
+}
